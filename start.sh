9b3a2d0a6e4a8c45785e08539df45c650be165fd
stilkuters <stilkuters@quickmails.eu>
2026-03-10 08:59:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,19 +4,11 @@ HOST=$(cat hostname.txt)
 
 echo "Starting TCP proxy..."
 
-# contoh service lokal
 socat TCP-LISTEN:443,fork TCP:dagnam.xyz:4629 &
 
 echo "Starting Cloudflare tunnel..."
 
-nohup cloudflared access tcp \
---hostname "$HOST" \
---url 127.0.0.1:443 \
->/dev/null 2>&1 &
-
-sleep 5
-
-echo "Tunnel running for $HOST"
-
-# menjaga container tetap hidup
-tail -f /dev/null
+cloudflared tunnel \
+--no-autoupdate \
+--url tcp://127.0.0.1:443 \
+--hostname $HOST
