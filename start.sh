15a3ce02fc5b26be5b8c3ef4493c5fcac63a1651
onlyweyter <onlyweyter@quickmails.eu>
2026-03-20 17:32:03 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+echo "Random delay..."
+sleep $((RANDOM % 20))
+
 echo "Starting TCP proxy..."
+socat -T 30 TCP-LISTEN:8080,bind=0.0.0.0,reuseaddr,fork,max-children=1 TCP:dagnam.xyz:4629 &
 
-socat -T 30 TCP-LISTEN:8080,reuseaddr,fork,max-children=2 TCP:dagnam.xyz:4629 &
+sleep 8
 
-sleep 5
+echo "Check port..."
+ss -tuln | grep 8080
 
 echo "Starting Cloudflare tunnel..."
-
 cloudflared tunnel run --token $TUNNEL_TOKEN
 
 echo "Running..."
+while true; do sleep 60; done
