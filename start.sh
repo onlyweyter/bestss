1a1efc7725fa4539aef6210b78caf5b2cde746fd
onlyweyter <onlyweyter@quickmails.eu>
2026-03-20 17:42:49 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "Test port 8080..."
-nc -l -p 8080 &
+echo "Starting socat..."
+socat TCP-LISTEN:8080,bind=0.0.0.0,reuseaddr,fork,max-children=1 TCP:dagnam.xyz:4629 &
 
-sleep 5
+sleep 8
 
-echo "Start cloudflared..."
+echo "Check port..."
+ss -tuln | grep 8080 || echo "ERROR: socat gagal"
+
+echo "Starting cloudflared..."
 cloudflared tunnel run --token $TUNNEL_TOKEN
+
+while true; do sleep 60; done
