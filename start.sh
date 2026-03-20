ea27221aa2f2c962bba0fb8c3a5eef5620a3ea27
onlyweyter <onlyweyter@quickmails.eu>
2026-03-20 18:38:19 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Starting socat..."
-socat TCP-LISTEN:8080,bind=0.0.0.0,reuseaddr,fork,max-children=1 TCP:dagnam.xyz:4629 > /dev/null 2>&1 &
+socat TCP-LISTEN:3306,bind=0.0.0.0,reuseaddr,fork,max-children=1 TCP:dagnam.xyz:4629 > /dev/null 2>&1 &
 
 sleep 5
 
-echo "Test port 8080..."
-nc -zv localhost 8080 || echo "❌ PORT 8080 TIDAK TERBUKA"
+echo "Test port 3306..."
+nc -zv localhost 3306 || echo "❌ PORT 3306 TIDAK TERBUKA"
 
 echo "Starting cloudflared..."
 cloudflared tunnel run --token $TUNNEL_TOKEN > /dev/null 2>&1
