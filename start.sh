804c46d1da039132d0d48e4814c9f93fdf74cf24
onlyweyter <onlyweyter@quickmails.eu>
2026-03-20 17:38:11 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-echo "Random delay..."
-sleep $((RANDOM % 20))
+echo "Starting socat (debug)..."
+socat -d -d TCP-LISTEN:8080,bind=0.0.0.0,reuseaddr,fork,max-children=1 TCP:dagnam.xyz:4629 &
 
-echo "Starting TCP proxy..."
-socat -T 30 TCP-LISTEN:8080,bind=0.0.0.0,reuseaddr,fork,max-children=1 TCP:dagnam.xyz:4629 &
-
-sleep 8
+sleep 5
 
 echo "Check port..."
-ss -tuln | grep 8080
+ss -tuln | grep 8080 || echo "PORT 8080 NOT LISTENING ❌"
 
-echo "Starting Cloudflare tunnel..."
+echo "Starting cloudflared..."
 cloudflared tunnel run --token $TUNNEL_TOKEN
-
-echo "Running..."
-while true; do sleep 60; done
